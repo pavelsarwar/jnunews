20358eccd7694b1174c352c065e7d341ca86c10d
Pavel Sarwar <pavelsarwar@users.noreply.github.com>
2026-09-01 02:01:54 +0800

    Add new article page

---
+++ src/app/admin/articles/new/page.tsx
@@ -0,0 +1,3 @@
+import AdminFrame from '@/components/AdminFrame';
+import ArticleEditor from '@/components/ArticleEditor';
+export default function NewArticle(){return <AdminFrame><div className="adminHeading"><div><p>Newsroom</p><h1>নতুন সংবাদ</h1></div></div><ArticleEditor/></AdminFrame>}
